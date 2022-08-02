3d0db0062aaf311483ce9bf0b829f5c729be744c
GitOfJY <maji6368@naver.com>
2022-08-02 20:48:57 +0900

    기본 프로젝트 구조

---
+++ src/main/java/com/spring/project/OutputController.java
@@ -0,0 +1,24 @@
+package com.spring.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OutputController {
+	
+	/*
+	 * private OutputService service;
+	 * 
+	 * @Autowired public OutputController(OutputService service) { this.service =
+	 * service; }
+	 */
+
+	@GetMapping(value="/test")
+	public String test() {
+		
+		return "test";
+	}
+	
+	
+}
